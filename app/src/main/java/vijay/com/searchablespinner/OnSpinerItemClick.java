4f80a6a3cb40a14d1b8756b088b599e02d32dc28
vijaynailwal1992 <vijaynailwal92@gmail.com>
2019-05-26 19:47:04 +0530

    Initial commit

---
+++ app/src/main/java/vijay/com/searchablespinner/OnSpinerItemClick.java
@@ -0,0 +1,10 @@
+package vijay.com.searchablespinner;
+
+/**
+ * Created by Md Farhan Raja on 2/23/2017.
+ */
+
+public interface OnSpinerItemClick
+{
+    public void onClick(String item, int position);
+}
